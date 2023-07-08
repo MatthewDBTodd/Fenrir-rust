f90ca8a766a0e504b0f3435cd29ddc7ac0d5e5b6
Matthew Todd <matthewdb.todd@gmail.com>
2023-07-08 21:39:41 +0100

    game by openings

--- fenrir_battleground2.py
+++ fenrir_battleground2.py
@@ -3,6 +3,41 @@ from dataclasses import dataclass
 import subprocess
 import sys
 import select
+from datetime import datetime
+
+openings = {
+    "English Opening" : ["c2c4"],
+    "Benoni" : ["d2d4", "g8f6", "c2c4", "c7c5", "d4d5"],
+    "Dutch Defence" : ["d2d4", "f7f5"],
+    "Reti Opening" : ["g1f3", "d7d5", "c2c4"],
+    "Benko Gambit" : ["d2d4", "g8f6", "c2c4", "c7c5", "d4d5", "b7b5"],
+    "Old Indian Defence" : ["d2d4", "g8f6", "c2c4", "d7d6"],
+    "Bird Opening" : ["f2f4"],
+    "Sicilian" : ["e2e4", "c7c5"],
+    "Caro-Kann" : ["e2e4", "c7c6"],
+    "Pirc Defence" : ["e2e4", "d7d6", "d2d4", "g8f6", "b1c3", "g7g6"],
+    "Alekhine's Defence" : ["e2e4", "g8f6"],
+    "Modern Defence" : ["e2e4", "g7g6"],
+    "Scandinavian Defence" : ["e2e4", "d7d5"],
+    "Ruy Lopez" : ["e2e4", "e7e5", "g1f3", "b8c6", "f1b5"],
+    "French Defence" : ["e2e4", "e7e6"],
+    "Petrov's Defence" : ["e2e4", "e7e5", "g1f3", "g8f6"],
+    "Vienna Game" : ["e2e4", "e7e5", "b1c3"],
+    "Centre Game" : ["e2e4", "e7e5", "d2d4", "e5d4"],
+    "King's Gambit" : ["e2e4", "e7e5", "f2f4"],
+    "Philidor Defence" : ["e2e4", "e7e5", "g1f3", "d7d6"],
+    "Italian Game" : ["e2e4", "e7e5", "g1f3", "b8c6", "f1c4"],
+    "Scotch Game" : ["e2e4", "e7e5", "g1f3", "b8c6", "d2d4"],
+    "Four Knights Game" : ["e2e4", "e7e5", "g1f3", "b8c6", "b1c3", "g8f6"],
+    "Queen's Gambit Accepted" : ["d2d4", "d7d5", "c2c4", "d5c4"],
+    "Queen's Gambit Declined" : ["d2d4", "d7d5", "c2c4", "e7e6"],
+    "Grunfeld Defence" : ["d2d4", "g8f6", "c2c4", "g7g6", "b1c3", "d7d5"], 
+    "Nimzo-Indian Defence" : ["d2d4", "g8f6", "c2c4", "e7e6", "b1c3", "f8b4"],
+    "Queen's Indian Defence" : ["d2d4", "g8f6", "c2c4", "e7e6", "g1f3", "b7b6"],
+    "King's Indian Defence" : ["d2d4", "g8f6", "c2c4", "g7g6"],
+    "Catalan Opening" : ["d2d4", "g8f6", "c2c4", "e7e6", "g2g3"],
+    "London System" : ["d2d4", "d7d5", "g1f3", "g8f6", "c1f4"],
+}
 
 @dataclass
 class EngineStats:
@@ -17,15 +52,76 @@ STALEMATE_CODE = 3
 THREEFOLD_CODE = 4
 FIFTYMOVE_CODE = 5
 
+def play_game_instance(engines, scores, white_idx, black_idx, opening_name, opening_moves):
+    current_turn_idx, other_turn_idx = white_idx, black_idx
+    for move in opening_moves:
+        engines[current_turn_idx].stdin.write(f"{move}")
+        engines[current_turn_idx].stdin.flush()
+        engines[other_turn_idx].stdin.write(f"{move}")
+        engines[other_turn_idx].stdin.flush()
+        _ = engines[current_turn_idx].stdout.readline()
+        _ = engines[other_turn_idx].stdout.readline()
+
+        current_turn_idx, other_turn_idx = other_turn_idx, current_turn_idx
+    while True:
+        engines[current_turn_idx].stdin.write(f"search time {ms}\n")
+        engines[current_turn_idx].stdin.flush()
+        move = engines[current_turn_idx].stdout.readline().strip()
+        print(f"{move}")
+        engines[other_turn_idx].stdin.write(f"{move}\n")
+        engines[other_turn_idx].stdin.flush()
+
+        confirm1 = engines[current_turn_idx].stdout.readline()
+        confirm2 = engines[other_turn_idx].stdout.readline()
+
+        if confirm1 == "ok\n" and confirm2 == "ok\n":
+            current_turn_idx, other_turn_idx = other_turn_idx, current_turn_idx
+        else:
+            parts = confirm1.split(":")
+            print(parts)
+            if len(parts) == 2 and parts[0] == "Game over":
+                code = int(parts[1].strip())
+
+                if code == WHITE_WIN_CODE:
+                    scores[white_idx].wins += 1
+                    scores[black_idx].losses += 1
+                elif code == BLACK_WIN_CODE:
+                    scores[black_idx].wins += 1
+                    scores[white_idx].losses += 1
+                elif code == STALEMATE_CODE or code == THREEFOLD_CODE or code == FIFTYMOVE_CODE:
+                    scores[white_idx].draws += 1
+                    scores[black_idx].draws += 1
+            pgn = engines[other_turn_idx].stdout.readline()
+            pgn_header = f"[Event \"Engine Battle with {opening_name}\"]\n"
+            current_date = datetime.now()
+            date_string = current_date.strftime("%Y.%m.%d")
+            pgn_header += f"[Date \{date_string}\"]\n"
+            pgn_header += "[White \"{}\"]\n".format(scores[white_idx].name)
+            pgn_header += "[Black \"{}\"]\n".format(scores[black_idx].name)
+            if code == WHITE_WIN_CODE:
+                pgn_header += "[Result \"1-0\"]\n"
+            elif code == BLACK_WIN_CODE:
+                pgn_header += "[Result \"0-1\"]\n"
+            else:
+                pgn_header += "[Result \"1/2-1/2\"]\n\n"
+            
+            pgn = pgn_header + pgn
+
+            with open(current_date, "w") as f:
+                f.write(pgn) 
+
+
+
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: fenrir_battleground.py engine1-path engin2-path ms-per-move")
+    if len(sys.argv) != 5:
+        print("Usage: fenrir_battleground.py engine1-path engine2-path games-per-opening ms-per-move")
         return
     
     
     engine1_path = sys.argv[1]
     engine2_path = sys.argv[2]
-    ms = int(sys.argv[3])
+    iterations = int(sys.argv[3])
+    ms = int(sys.argv[4])
 
     engine1 = subprocess.Popen(
         [engine1_path, "-q"],
@@ -45,11 +141,6 @@ def main():
 
     white_idx, black_idx = 0, 1
 
-    current_turn_idx, other_turn_idx = white_idx, black_idx
-
-
-
-
     engine1_version = engine1.stdout.readline().strip()
     engine2_version = engine2.stdout.readline().strip()
     print(f'Engine 1 is {engine1_version}')
@@ -57,43 +148,11 @@ def main():
 
     scores = [EngineStats(name=engine1_version), EngineStats(name=engine2_version)]
 
-    while True:
-        engines[current_turn_idx].stdin.write(f"search time {ms}\n")
-        engines[current_turn_idx].stdin.flush()
-        move = engines[current_turn_idx].stdout.readline().strip()
-        print(f"move = {move}")
-        engines[other_turn_idx].stdin.write(f"{move}\n")
-        engines[other_turn_idx].stdin.flush()
-
-        reads, _, _ = select.select([engines[other_turn_idx].stdout], [], [], 0.2)
-
-        if reads:
-            # There's something to read, so read a line
-            line = engines[other_turn_idx].stdout.readline()
-            parts = line.split(":")
-            print(parts)
-            if len(parts) == 2 and parts[0] == "Game over":
-                print("game is over")
-                code = int(parts[1].strip())
-                print(f"code = {code}")
-
-                if code == WHITE_WIN_CODE:
-                    scores[white_idx].wins += 1
-                    scores[black_idx].losses += 1
-                elif code == BLACK_WIN_CODE:
-                    scores[black_idx].wins += 1
-                    scores[white_idx].losses += 1
-                elif code == STALEMATE_CODE or code == THREEFOLD_CODE or code == FIFTYMOVE_CODE:
-                    scores[white_idx].draws += 1
-                    scores[black_idx].draws += 1
-            
-            print("asdf")
-            pgn = engines[other_turn_idx].stdout.readline()
-            print("asdfsdaf")
-            print(pgn)
-            break
-
-        current_turn_idx, other_turn_idx = other_turn_idx, current_turn_idx
+    for opening_name, opening_moves in openings.items():
+        for _ in range(iterations):
+            play_game_instance(engines, scores, white_idx, black_idx, opening_name, opening_moves)
+            white_idx, black_idx = black_idx, white_idx
+            play_game_instance(engines, scores, white_idx, black_idx, opening_name, opening_moves)
 
     print(scores)
 
